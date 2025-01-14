94a863f026fee4c7429e0fdd2f113b2b496fb5d2
Mominul Islam <mominul.cse21@gmail.com>
2025-01-14 20:50:05 +0600

    Change the banner and footer

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,16 +3,19 @@ import logo from "../assets/cselogo.png";
 
 const Banner = () => {
   return (
-    <div
-      className="relative flex items-center bg-blue-100 bg-center">
-      <div>
-        <img src={logo} alt="" className="object-fill" />
+    <div className="relative flex flex-col items-center p-6 bg-blue-100 bg-center md:flex-row md:justify-around md:p-10 bg-opacity-50 z-10  bg-[url('https://i.pinimg.com/564x/cb/be/72/cbbe720dfad485f4aa2f561959abb7b8.jpg')]">
+      <div className="mb-6 md:mb-0">
+        <img
+          src={logo}
+          alt="University Logo"
+          className="object-fill max-w-xs md:max-w-md"
+        />
       </div>
-      <div>
-        <h1 className="text-6xl font-bold">
+      <div className="text-center md:text-left">
+        <h1 className="text-4xl font-bold md:text-6xl">
           First Capital University of Bangladesh
         </h1>
-        <p className="mt-3 text-4xl">Chuadanga</p>
+        <p className="mt-3 text-2xl md:text-4xl">Chuadanga</p>
       </div>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 w-full p-4 mt-5 text-center text-white bg-gray-800">
-      GPA Calculator. All rights reserved.
+    <div className="fixed bottom-0 left-0 flex justify-center w-full gap-4 p-4 mt-10 text-center text-white bg-gray-800">
+      <div className="text-center text-gray-500 text-md">
+        All rights reserved © 2024 BookSwap
+      </div>
+      <div className="flex items-center justify-center">
+        <a
+          href="https://www.instagram.com/m.mominull/"
+          target="_blank"
+          className=" hover:text-gray-900"
+        >
+          <FaInstagram className="w-6 h-8" />
+        </a>
+      </div>
     </div>
   );
 };
